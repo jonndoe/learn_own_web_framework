4482454141d95a7311b706735cf7fb112da414f3
jonndoe <lukichevvasily@gmail.com>
2023-12-14 13:28:18 +0300

    Added request handlers, simple and parameterized routing

--- mironodi/app.py
+++ mironodi/app.py
@@ -1,3 +1,24 @@
 from api import API
 
 app = API()
+
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from the HOME page"
+
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from ABOUT page"
+
+
+@app.route("/hello/{name}")
+def greeting(request, response, name):
+    response.text = f"Hello, {name}"
+
+
+@app.route("/sum/{num_1:d}/{num_2:d}")
+def sum(request, response, num_1, num_2):
+    total = int(num_1) + int(num_2)
+    response.text = f"{num_1} + {num_2} = {total}"
